5897d72d7e8f7d6a99220a6d5fe5a187511a4035
Solo Abramov <59119335+soloAbramov@users.noreply.github.com>
2021-07-01 14:14:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const form = document.querySelector(".main-container form");
+const input = document.querySelector(".text");
+const search = document.querySelector(".search");
+const msg = document.getElementById("msg");
+const list = document.querySelector(".api-container .cities");
+
+const apiKey = "dc0a697d7d99d9e88f2575a767e75543";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let inputVal = input.value;
+
+  const listItems = list.querySelectorAll(".api-container .city");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter((el) => {
+      let content = "";
+
+      if (inputVal.includes(",")) {
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      }. Otherwise be more specific by providing the country code as well`;
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+          <h2 class="city-name" data-name="${name},${sys.country}">
+            <span>${name}</span>
+            <sup>${sys.country}</sup>
+          </h2>
+          <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+          <figure>
+            <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+            <figcaption>${weather[0]["description"]}</figcaption>
+          </figure>
+        `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
